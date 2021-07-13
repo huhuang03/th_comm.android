b48946939e14ea06398d40c4815695523265471d
huhuang03 <huhuang03@gmail.com>
2021-07-13 09:31:14 +0800

    add maven config, but haven't config the sign info

--- app/src/main/java/com/th/android/comm/sample/MainActivity.kt
+++ app/src/main/java/com/th/android/comm/sample/MainActivity.kt
@@ -3,7 +3,6 @@ package com.th.android.comm.sample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.th.android.comm.TLog
-import com.th.android.comm.log
 import com.th.android.comm.tlog
 
 class MainActivity : AppCompatActivity() {
---
+++ th_comm/src/main/java/com/th/android/comm/inner/inner_log.kt
@@ -0,0 +1,1 @@
+package com.th.android.comm.inner
