976b2e1063ce13ba065e2deeba90e665fd73d288
huhuang03 <huhuang03@gmail.com>
2021-07-28 09:45:34 +0800

    up

--- th_comm/src/main/java/com/th/android/comm/util/util_sp.kt
+++ th_comm/src/main/java/com/th/android/comm/util/util_sp.kt
@@ -4,6 +4,16 @@ import android.content.Context
 
 const val SP_DEFAULT_FILE = "config"
 
+fun spSaveBool(context: Context, key: String, value: Boolean, spFileName: String = SP_DEFAULT_FILE) {
+    context.getSharedPreferences(spFileName, Context.MODE_PRIVATE)
+        .edit().putBoolean(key, value).apply()
+}
+
+fun spGetBool(context: Context, key: String, defValue: Boolean = false, spName: String = SP_DEFAULT_FILE): Boolean {
+    return context.getSharedPreferences(spName, Context.MODE_PRIVATE)
+        .getBoolean(key, defValue)
+}
+
 fun spSaveStr(context: Context, key: String, value: String, spFileName: String = SP_DEFAULT_FILE) {
     context.getSharedPreferences(spFileName, Context.MODE_PRIVATE)
         .edit().putString(key, value).apply()
